4365a7ce6294c243984e993b75bb9d3d21ae0f83
delasous <sousa.yak@googlemail.com>
2022-02-26 14:31:20 +0100

    Tidy rollup config and package to remove unused plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import svelte from 'rollup-plugin-svelte';
-import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-import livereload from 'rollup-plugin-livereload';
+import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
 import nodePolyfills from 'rollup-plugin-node-polyfills';
@@ -9,27 +8,6 @@ import nodePolyfills from 'rollup-plugin-node-polyfills';
 import copy from './rollup-plugin-copy';
 
 const production = !process.env.ROLLUP_WATCH;
-
-function serve() {
-	let server;
-
-	function toExit() {
-		if (server) server.kill(0);
-	}
-
-	return {
-		writeBundle() {
-			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
-			});
-
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
-	};
-}
 
 export default [{
 		input: 'src/app.js',
@@ -51,8 +29,6 @@ export default [{
 				dedupe: ['svelte']
 			}),
 			commonjs(),
-			!production && serve(),
-			!production && livereload('dist'),
 			production && terser(),
 			copy({ 
 				srcFolder: './public/', 
@@ -92,8 +68,6 @@ export default [{
 				dedupe: ['svelte']
 			}),
 			commonjs(),
-			!production && serve(),
-			!production && livereload('dist'),
 			production && terser(),
 		],
 		watch: {
@@ -120,8 +94,6 @@ export default [{
 				dedupe: ['svelte']
 			}),
 			commonjs(),
-			!production && serve(),
-			!production && livereload('dist'),
 			production && terser(),
 		],
 		watch: {
